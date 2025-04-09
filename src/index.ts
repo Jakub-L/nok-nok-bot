4f3d1b016b06b40718cb22e09fd0e29b2e7bc78d
Jakub L. <47220449+Jakub-L@users.noreply.github.com>
2025-04-09 22:50:10 +0100

    Set up basic Discord bot

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello, world!');
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+import 'dotenv/config';
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (readyClient) => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
