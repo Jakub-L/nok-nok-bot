127090c9fd695a108c5837ecb781dc48ef0497e3
Jakub L. <47220449+Jakub-L@users.noreply.github.com>
2025-04-09 22:34:16 +0100

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import prettier from 'eslint-config-prettier';
+import js from '@eslint/js';
+import { includeIgnoreFile } from '@eslint/compat';
+import globals from 'globals';
+import { fileURLToPath } from 'node:url';
+import ts from 'typescript-eslint';
+const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
+
+export default ts.config(
+  includeIgnoreFile(gitignorePath),
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  prettier,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    }
+  },
+  {
+    ignores: ['eslint.config.js', 'svelte.config.js'],
+
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        parser: ts.parser
+      }
+    }
+  }
+);
