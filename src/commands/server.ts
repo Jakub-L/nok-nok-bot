ac07fca61d1d94e912874e2decf92782035d75b7
Jakub L. <47220449+Jakub-L@users.noreply.github.com>
2025-04-11 22:19:27 +0100

    Remove discord.js since it doesn't support cloudflare workers deployment

--- src/commands/server.ts
+++
@@ -1,9 +0,0 @@
-import { SlashCommandBuilder } from 'discord.js';
-import type { ChatInputCommandInteraction } from 'discord.js';
-
-export const server = {
-	data: new SlashCommandBuilder().setName('server').setDescription('Sets server IP'),
-	async execute(interaction: ChatInputCommandInteraction) {
-		await interaction.reply({ content: 'I got your request' });
-	}
-};
