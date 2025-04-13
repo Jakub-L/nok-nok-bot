8089b139bdb5314de8501c2c9c045b64785efe09
Jakub L. <47220449+Jakub-L@users.noreply.github.com>
2025-04-13 21:53:52 +0100

    Add basic response to interactions

---
+++ dist/commands.js
@@ -0,0 +1,9 @@
+const SET_SERVER = {
+    name: 'server',
+    description: 'Post the current Foundry server IP address and remove previous links to prevent confusion.'
+};
+const REMINDER = {
+    name: 'reminder',
+    description: 'Posts a game reminder, letting the players know whether there is a game today or not.'
+};
+export const commands = [SET_SERVER, REMINDER];
--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,67 @@
 import { AutoRouter } from 'itty-router';
+import { InteractionResponseType, InteractionType, verifyKey } from 'discord-interactions';
 
+// UTILS
+/** JSON Response to a request */
 class JsonResponse extends Response {
-	constructor(body: any, init: Record<string, any>) {
-		const jsonBody = JSON.stringify(body);
-		super(
-			jsonBody,
-			init || {
-				headers: {
-					'content-type': 'application/json;charset=UTF-8'
-				}
+	constructor(
+		body: any,
+		init: Record<string, any> = {
+			headers: {
+				'content-type': 'application/json;charset=UTF-8'
 			}
-		);
+		}
+	) {
+		const jsonBody = JSON.stringify(body);
+		super(jsonBody, init);
 	}
 }
 
+/**
+ * Checks if the request is a valid Discord interaction
+ * @param {Request} request - Incoming request
+ * @param {any} env - Environment variables
+ * @returns {Promise<{ interaction: any, isValid: boolean }>} - Returns the interaction and whether
+ * 				the request is valid
+ */
+async function verifyDiscordRequest(request: Request, env: any) {
+	const signature = request.headers.get('x-signature-ed25519');
+	const timestamp = request.headers.get('x-signature-timestamp');
+	const body = await request.text();
+	const isValidRequest =
+		signature && timestamp && (await verifyKey(body, signature, timestamp, env.DISCORD_PUBLIC_KEY));
+	if (!isValidRequest) return { isValid: false };
+	return { interaction: JSON.parse(body), isValid: true };
+}
+
+// ROUTING
 const router = AutoRouter();
 
-router.get('/', (_, env) => {
-	console.log(env);
-	return new Response(`👋 ${env.DISCORD_APPLICATION_ID}`);
+/** Basic route to see worker is active */
+router.get(
+	'/',
+	(_, env: any) => new Response(`Running Discord application ID: ${env.DISCORD_APPLICATION_ID}`)
+);
+
+/** Discord interaction endpoint */
+router.post('/', async (req: Request, env: any) => {
+	const { isValid, interaction } = await verifyDiscordRequest(req, env);
+	if (!isValid || !interaction) return new Response('Bad request signature.', { status: 401 });
+
+	if (interaction.type === InteractionType.PING) {
+		return new JsonResponse({
+			type: InteractionResponseType.PONG
+		});
+	} else if (interaction.type === InteractionType.APPLICATION_COMMAND) {
+		return new JsonResponse({
+			type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+			data: { content: 'Hello World!' }
+		});
+	} else return new JsonResponse({ error: 'Unknown Type' }, { status: 400 });
 });
+
+/** All other routes return 404 */
+router.all('*', () => new Response('Not Found', { status: 404 }));
 
 export const server = {
 	fetch: router.fetch
