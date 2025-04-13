0e92477ccd620a53b9035a09f69539965e3d0cb5
Jakub L. <47220449+Jakub-L@users.noreply.github.com>
2025-04-13 22:11:02 +0100

    Add dist to gitignore

--- dist/commands.js
+++
@@ -1,9 +0,0 @@
-const SET_SERVER = {
-    name: 'server',
-    description: 'Post the current Foundry server IP address and remove previous links to prevent confusion.'
-};
-const REMINDER = {
-    name: 'reminder',
-    description: 'Posts a game reminder, letting the players know whether there is a game today or not.'
-};
-export const commands = [SET_SERVER, REMINDER];
