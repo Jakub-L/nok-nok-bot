4a8dc50c92083adff5d853b3c4ec9b2948da6dff
Jakub L <47220449+Jakub-L@users.noreply.github.com>
2025-04-19 23:34:25 +0200

    Allow explicit any

--- eslint.config.js
+++ eslint.config.js
@@ -7,26 +7,28 @@ import ts from 'typescript-eslint';
 const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
 export default ts.config(
-  includeIgnoreFile(gitignorePath),
-  js.configs.recommended,
-  ...ts.configs.recommended,
-  prettier,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node
-      }
-    }
-  },
-  {
-    ignores: ['eslint.config.js', 'svelte.config.js'],
-
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        parser: ts.parser
-      }
-    }
-  }
+	includeIgnoreFile(gitignorePath),
+	js.configs.recommended,
+	...ts.configs.recommended,
+	prettier,
+	{
+		rules: {
+			'@typescript-eslint/no-explicit-any': 'off'
+		},
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node
+			}
+		}
+	},
+	{
+		ignores: ['eslint.config.js', 'svelte.config.js'],
+		languageOptions: {
+			parserOptions: {
+				projectService: true,
+				parser: ts.parser
+			}
+		}
+	}
 );
